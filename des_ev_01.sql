0ac7b27ad42dfe742f0ed3b07260f80b4127f540
MariaJoseWelkner <josewelkner@gmail.com>
2022-07-05 12:14:56 -0400

    agregando desafio

---
+++ des_ev_01.sql
@@ -0,0 +1,30 @@
+-- desafio peliculas y reparto
+
+-- preg 3:
+
+SELECT  id
+	FROM public.peliculas
+    where pelicula='Titanic';
+
+-- preg 4: 
+SELECT * FROM reparto WHERE ID=2 
+
+-- preg 5:
+SELECT count(id) FROM reparto WHERE actor='Harrison Ford'
+
+-- preg 6:
+
+
+
+
+
+-- preg 7:
+
+
+
+
+-- preg 8:
+
+
+
+
